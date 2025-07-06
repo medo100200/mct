c388c66e5dca3e2f7d52c069ee48d4d850b09299
Mahmoud Yousef <mct100200@gmail.com>
2025-07-06 16:21:01 +0300

    prepare for Render deployment

--- app.py
+++ app.py
@@ -289,8 +289,11 @@ def search():
         ).all()
     return render_template('search.html', devices=devices)
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 
 
